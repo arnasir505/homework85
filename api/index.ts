3aae8ddf3cd3585ffd8242bab6cefefa05b17af1
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-04-25 19:43:46 +0600

    Use express static to serve images

--- api/index.ts
+++ api/index.ts
@@ -13,6 +13,9 @@ const port = 8000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
+app.use(express.static('fixtures'));
+
 app.use('/artists', artistsRouter);
 app.use('/albums', albumsRouter);
 app.use('/tracks', tracksRouter);
