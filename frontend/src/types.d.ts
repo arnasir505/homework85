982695426dabc3bdb7277fad27eb347e7fa6a4e9
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-04-26 13:20:44 +0600

    Create artist, album and track interfaces

---
+++ frontend/src/types.d.ts
@@ -0,0 +1,22 @@
+export interface Artist {
+  _id: string;
+  name: string;
+  information: string;
+  image: string;
+}
+
+export interface Album {
+  _id: string;
+  title: string;
+  artist: string;
+  year: number;
+  image: string;
+}
+
+export interface Track {
+  _id: string;
+  title: string;
+  album: string;
+  duration: string;
+  position: number;
+}
